da0b4c6a4ef10208243302073fbf8d149baa115f
Adil Naseer Khawaja <adildhkh@gmail.com>
2025-08-09 20:34:31 +0500

    Add acceptance_test job to lesson-6.yaml

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
@@ -1,22 +1,22 @@
 describe('Hits Home Page', () => {
   it('passes', () => {
-    cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/');
+    cy.visit('http://localhost:3000/');
   });
+
   it('displays the Welcome message', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are is a Welcome message.
     cy.get('h1').should(
       'have.text',
       "Welcome to Hangry Hippo! Hungry? Let's get started!"
     );
   });
+
   it('shows the Menu items', () => {
-    //Check for the Appeteasers menu item
+    // Check for the Appeteasers menu item
     cy.get('[data-testid=category-item]')
       .should('be.visible')
       .and('contain', 'Appeteasers');
 
-    //Check for the Handhelds menu item
+    // Check for the Handhelds menu item
     cy.get('[data-testid=category-item]')
       .should('be.visible')
       .and('contain', 'Handhelds');
@@ -25,11 +25,10 @@ describe('Hits Home Page', () => {
 
 describe('Hits Order Page', () => {
   it('passes', () => {
-    cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/order');
+    cy.visit('http://localhost:3000/order');
   });
+
   it('shows Your Order title', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    //Check to see if 'Your Order' exists
     cy.get('h3').should('have.text', 'Your Order');
   });
 });
